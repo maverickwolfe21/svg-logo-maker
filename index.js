d5f7905bc7a97044cae4f27ec7012434c7968c4d
Maverick Wolfe <maverick.m.wolfe@gmail.com>
2024-02-07 08:42:32 -0700

    setup writeToFile using shapes class and inquirer

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 
+const { Circle, Triangle, Square } = require("./lib/shapes"); //import shapes class
+
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
   fs.writeFile(fileName, data, (err) => (err ? console.error(err) : console.log("Success!")));
@@ -16,11 +18,12 @@ function init() {
         type: "input",
         message: "Enter text (3 characters max):",
         name: "text",
+        validate: (input) => /^[A-Za-z0-9]{1,3}$/.test(input), // regex for validation
       },
       {
         type: "input",
         message: "Enter text color (keyword or hexadecimal):",
-        name: "text-color",
+        name: "textColor",
       },
       {
         type: "list",
@@ -31,12 +34,36 @@ function init() {
       {
         type: "input",
         message: "Enter shape color (keyword or hexadecimal):",
-        name: "shape-color",
+        name: "shapeColor",
       },
     ])
 
     .then((response) => {
-      writeToFile("logo.svg", "content");
+      //define height and width of svg
+      const sHeight = 200;
+      const sWidth = 300;
+
+      // create shape from user input
+      let shape;
+
+      if (response.shape === "Circle") {
+        shape = new Circle(response.shapeColor);
+      } else if (response.shape === "Triangle") {
+        shape = new Triangle(response.shapeColor);
+      } else {
+        shape = new Square(response.shapeColor);
+      }
+
+      // create SVG based on specified height and width for the shape (w3 schools reference)
+      const svg = `<svg xmlns="http://www.w3.org/2000/svg" height="${sHeight}" width="${sWidth}">
+    ${shape.render()} <!-- Render the selected shape -->
+    <text x="150" y="125" font-size="60" text-anchor="middle" fill="${response.textColor}">${
+        response.text
+      }</text> <!-- Add the text and set the color -->
+  </svg>`;
+
+      // create actual svg file
+      writeToFile("examples/logo.svg", svg);
     });
 }
 
